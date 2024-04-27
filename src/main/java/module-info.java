9c0bbb948a0661c1153f08a62903f05db0740272
andreavalfonzo <andreavalfonzo13@gmail.com>
2024-04-27 14:39:33 -0400

    Initial Commit from IntelliJ

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.habittracker {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.habittracker to javafx.fxml;
+    exports com.example.habittracker;
+}
